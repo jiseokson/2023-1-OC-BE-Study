e42f9081b61352d0e27e5f0a7f582c237f8bd001
Jiseok Son <jiseokson@gmail.com>
2023-04-05 14:06:19 +0900

    Week3: JdbcTemplate 이용한 구현

--- hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -2,12 +2,14 @@ package hello.hellospring;
 
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.JdbcMemberRepository;
+import hello.hellospring.repository.JdbcTemplateMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 import javax.sql.DataSource;
 import javax.swing.*;
@@ -28,8 +30,6 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-
-//        return new MemoryMemberRepository();
-        return new JdbcMemberRepository(dataSource);
+        return new JdbcTemplateMemberRepository(dataSource);
     }
 }
